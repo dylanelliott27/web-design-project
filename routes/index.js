e13c117fda09e54e58b9f09518a94e2b1e1666f7
Dylan <dylanelliott27@hotmail.com>
2021-08-11 05:21:46 +0000

    sd

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/rachana', function(req, res, next) {
+  res.render('rachana');
+});
+
+router.get('/dylan', function(req, res, next) {
+  res.render('dylan');
+});
+
+router.get('/disha', function(req, res, next) {
+  res.render('disha');
+});
+
+router.get('/tanupreet', function(req, res, next) {
+  res.render('tanupreet');
+});
+
+router.get('/kulpreet', function(req, res, next) {
+  res.render('kulpreet');
+});
+
 module.exports = router;
